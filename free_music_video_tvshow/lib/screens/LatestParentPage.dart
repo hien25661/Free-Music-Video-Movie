32f04cb1fcceb667e98cae49843c39a98bb5fcec
Hiennguyen <hiennv@its-j.com>
2019-09-16 15:12:11 +0900

    Latest screen

--- free_music_video_tvshow/lib/screens/LatestParentPage.dart
+++ free_music_video_tvshow/lib/screens/LatestParentPage.dart
@@ -7,6 +7,44 @@ class LatestParentPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.orange);
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: PreferredSize(
+            child: Container(
+              height: 50.0,
+              child: TabBar(tabs: [
+                Tab(
+                  text: "24 Hours",
+                ),
+                Tab(
+                  text: "7 Days",
+                ),
+                Tab(
+                  text: "Weekly Chart",
+                ),
+                Tab(
+                  text: "Monthly Chart",
+                )
+              ],
+              isScrollable: true,),
+            ),
+            preferredSize: Size.fromHeight(50.0)),
+        body: TabBarView(children: [
+          Container(
+            color: Colors.white,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.blueAccent,
+          ),
+          Container(
+            color: Colors.red,
+          )
+        ]),
+      ),
+    );
   }
 }
